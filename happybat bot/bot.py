9def594b2e421d0e4d524e85690be4ea272ce45f
HappyBat1189 <62520724+HappyBat1189@users.noreply.github.com>
2020-03-22 16:16:59 -0400

    Update bot.py

--- happybat bot/bot.py
+++ happybat bot/bot.py
@@ -115,4 +115,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('NjkwNzE1NjYwNjYxMjkzMDY2.XnV0Zw.0e9mGIi-Lirv_c4OYlDJ47nRiLY')
+client.run('YOUR TOKEN HERE!')
